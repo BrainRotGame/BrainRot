afc4690c497c7dd81e93408d2aa10327a392d589
Raio Chea <56056833+Raio0517@users.noreply.github.com>
2024-11-21 11:10:46 -0800

    game view is fairly closed to finished, functionality still a WIP

--- lib/providers/game_state_provider.dart
+++ lib/providers/game_state_provider.dart
@@ -3,19 +3,23 @@ import 'package:flutter/material.dart';
 //GameStateProvider will keep track of the game state when a game's being played
 //Provider will maintain score and time of the current game
 class GameStateProvider extends ChangeNotifier{
+
   int time;
   int correct;
   int skipped;
+  // List<Word> words;
 
   GameStateProvider({required this.time}) : correct = 0, skipped = 0;
 
   void incrementCorrect() {
     correct++;
+    // words.removeLast();
     notifyListeners();
   }
 
   void incrementSkip() {
     skipped++;
+    // words.removeLast();
     notifyListeners();
   }
 
--- lib/views/game_views/game_view.dart
+++ lib/views/game_views/game_view.dart
@@ -1,5 +1,7 @@
+import 'package:brainrot/providers/game_state_provider.dart';
 import 'package:brainrot/views/game_views/draw_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GameView extends StatelessWidget {
 
@@ -7,15 +9,52 @@ class GameView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(onPressed: () => _navigateToDrawing(context), 
-          child: const Text("Guessed Correct")),
-        ElevatedButton(onPressed: () => _navigateToDrawing(context), 
-          child: const Text("Navigate to drawing")),
-          ElevatedButton(onPressed: () => _navigateToDrawing(context), 
-          child: const Text("Skip")),
-      ],
+    return Consumer<GameStateProvider>(
+      builder: (context, gameStateProvider, child) {
+          return Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                ElevatedButton(onPressed: () => gameStateProvider.incrementCorrect(), 
+                  child: const Text("Guessed Correct")),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(5),
+                          color: const Color.fromARGB(255, 197, 235, 253),
+                        ),
+                        child: Column(children: [
+                          Text('Correct Guesses: ${gameStateProvider.correct}'),
+                          Text('Skipped: ${gameStateProvider.skipped}'),
+                        ],),
+                      ),
+                      // const SizedBox(width: 300),
+                      const Expanded(child: SizedBox()),
+                      const Text("TEXT", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+                      const Expanded(child: SizedBox()),
+                      // const SizedBox(width: 300),
+                      ElevatedButton(onPressed: () => _navigateToDrawing(context), 
+                      child: const Text("Navigate to drawing")),
+                    ]
+                    
+                  ),
+                ),
+                
+                  ElevatedButton(onPressed: () => gameStateProvider.incrementSkip(), 
+                  child: const Text("Skip")),
+              ],
+            ),
+          ),
+        );
+      }
       
     );
   }
