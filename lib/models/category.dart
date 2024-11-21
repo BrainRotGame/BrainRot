8c81908681a37f06046371080537db8c8111bc28
Yijia <60768920+Jasmine0814@users.noreply.github.com>
2024-11-20 18:08:20 -0800

    build data model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,1 +1,17 @@
 // a list of words in the same category
+
+import 'package:brainrot/models/word.dart';
+
+class Category {
+
+  final List<Word> _category;
+
+  // constructor
+  Category() : _category = [];
+
+
+  // Getter method for category
+  List<Word> get category => List.from(_category);
+
+}
+
--- lib/models/word.dart
+++ lib/models/word.dart
@@ -1,3 +1,15 @@
 // word name
 // description
 // hint(optional)
+
+class Word {
+  final String word;
+  final String description;
+  final String? hint;
+
+  Word({
+    required this.word,
+    required this.description,
+    this.hint,
+  });
+}
