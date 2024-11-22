84b6091b1f78583b26fcc5fb5e3223ea1d266e2b
Raio Chea <56056833+Raio0517@users.noreply.github.com>
2024-11-22 10:06:06 -0800

    Reimplement back button and added game state display in the draw view

--- lib/views/game_views/draw_view.dart
+++ lib/views/game_views/draw_view.dart
@@ -5,8 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class DrawView extends StatelessWidget {
-  const DrawView({super.key, required this.width, required this.height});
+  const DrawView({super.key, required this.width, required this.height, required this.guessed, required this.skipped});
 
+  final int guessed;
+  final int skipped;
   final double width;
   final double height;
 
@@ -19,15 +21,43 @@ class DrawView extends StatelessWidget {
               Semantics(label: 'Clear all button',excludeSemantics: true, child: IconButton(key: const Key('Clear'), iconSize: 30, onPressed: () => _clear(context), icon: const Icon(Icons.clear))),
               Semantics(label: 'Undo button',excludeSemantics: true, child: IconButton(key: const Key('Undo'), iconSize: 30, onPressed: () => _undo(context), icon: const Icon(Icons.undo))),
               Semantics(label: 'Redo button',excludeSemantics: true, child: IconButton(key: const Key('Redo'), iconSize: 30, onPressed: () => _redo(context), icon: const Icon(Icons.redo))),
-              Semantics(label: 'Back button',excludeSemantics: true, child: IconButton(key: const Key('Back'), iconSize: 30, onPressed: () => _popBack(context), icon: const Icon(Icons.arrow_back))),
         ]),
         drawer: Drawer(
           child: Palette(context, key: const Key('Palette')),
         ),
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-            child: DrawArea(width: width, height: height),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: 200,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5),
+                    color: const Color.fromARGB(255, 197, 235, 253),
+                  ),
+                  child: Column(children: [Text('Correct Guesses: $guessed'),Text('Skipped: $skipped'),],)),
+              ),
+              Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                child: DrawArea(width: width, height: height),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Semantics(
+                  label: 'Back button',
+                  excludeSemantics: true, 
+                  child: ElevatedButton(
+                    onPressed: () => _popBack(context), 
+                    style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 192, 235, 255)), foregroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 36, 36, 36))),
+                    child: const Icon(Icons.arrow_back, size: 30,)))
+                  // child: IconButton(key: const Key('Back'), iconSize: 30, onPressed: () => _popBack(context), icon: const Icon(Icons.arrow_back)))
+                ),
+            ]
           ),
         ),
       ),
